0ba7e610cfad1653c1d6d21692a4bc4b27c071d8
SGP0410 <2742837998@qq.com>
2021-03-29 09:38:42 +0800

    1

--- src/main/java/com/example/soph/dao/Userdao.java
+++ src/main/java/com/example/soph/dao/Userdao.java
@@ -2,7 +2,7 @@ package com.example.soph.dao;
 
 /**
  * @author 孙国鹏
- * @date 2021/3/29 8:10
+ * @date 2021/3/29 9:18
  */
-public class aaa {
+public interface Userdao {
 }
---
+++ src/main/java/com/example/soph/dao/impl/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package com.example.soph.dao.impl;
+
+import com.example.soph.dao.Userdao;
+
+/**
+ * @author 孙国鹏
+ * @date 2021/3/29 9:19
+ */
+public class UserDaoImpl extends BaseDao implements Userdao {
+
+}
